358a9bb1c20021cacbff8c0584c05b390237507d
TheryJohanna <theresa.genssler@web.de>
2023-06-21 15:01:24 +0200

    Tried to fix skybox (still not working)

--- framework/source/geometry_node.cpp
+++ framework/source/geometry_node.cpp
@@ -161,8 +161,19 @@ void GeometryNode::renderNode(const std::map<std::string, shader_program> &m_sha
 void GeometryNode::renderSkybox(const std::map<std::string, shader_program> &m_shaders,
                                 const glm::mat4 &m_view_transform) const {
     glUseProgram(m_shaders.at("skybox").handle);
+    glm::fmat4 model_matrix = getWorldTransform() * getLocalTransform();
+
+    glUniformMatrix4fv(m_shaders.at("skybox").u_locs.at("ModelMatrix"),
+                       1, GL_FALSE, glm::value_ptr(model_matrix));
+
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_CUBE_MAP, texture_.handle);
     glUniform1i(m_shaders.at("skybox").u_locs.at("TextureSampler"), 0);
+
+    // bind the VAO to draw
+    gl::glBindVertexArray(geometry_.vertex_AO);
+
+    // draw bound vertex array using bound shader
+    glDrawElements(geometry_.draw_mode, geometry_.num_elements, model::INDEX.type, NULL);
 }
 
